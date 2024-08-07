398c5f260f142871d07954819b7dcef9fdc1dea3
Nicolas Vouilloux <nicolasvcire@gmail.com>
2024-08-07 22:28:50 +0200

    starting the setbomb for the minesweeper

--- JS-HTML-CSS/MineSweeper/script.js
+++ JS-HTML-CSS/MineSweeper/script.js
@@ -1,5 +1,6 @@
 let newGameBTN = document.getElementById("NewGameButton");
 let nbSize = document.getElementById("MineSweeperSize");
+let nbBomb = document.getElementById("MineSweeperBomb");
 let body = document.body;
 
 function setSize(){
@@ -9,12 +10,22 @@ function setSize(){
         tbl.appendChild(tr);
         for(let j=0; j<nbSize.value;j++){
             let td = document.createElement("td");
+            let btn = document.createElement("button");
+            btn.innerText = "    ";
+            td.appendChild(btn);
             tr.appendChild(td);
         }
     }
     body.appendChild(tbl);
 }
-
+function setBomb(){
+    for (let i=0; i<nbBomb.value;i++){
+        
+    }
+}
+function randomInt(){
+    return Math.floor(math.random * (nbSize.value));
+}
 
 
 function newGame(){
