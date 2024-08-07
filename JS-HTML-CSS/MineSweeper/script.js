39d1d82e1509bb07d8e46c63fff3f0061d8d1daf
Nicolas Vouilloux <nicolasvcire@gmail.com>
2024-08-07 21:27:48 +0200

    Starting JS

---
+++ JS-HTML-CSS/MineSweeper/script.js
@@ -0,0 +1,7 @@
+let bombBTN = document.getElementById("BombButton");
+let sizeBTN = document.getElementById("SizeButton");
+
+function modifieSize(){
+    let nbSize = document.getElementById("MineSweeperSize").value;
+}
+bombBTN.addEventListener("click",modifieSize);
