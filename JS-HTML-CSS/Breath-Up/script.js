baec216de84f62e3a1f0707a92cc47f5b41d0bcc
Nicolas Vouilloux <nicolasvcire@gmail.com>
2024-08-15 21:32:21 +0200

    clarrify the code

--- JS-HTML-CSS/Breath-Up/script.js
+++ JS-HTML-CSS/Breath-Up/script.js
@@ -3,6 +3,7 @@ let selectedContainer = starting;
 let btnStart = document.getElementById("startButton");
 let circle = document.getElementById("circle")
 let progress = document.querySelector(".tracker");
+let iteration = 1;
 
 const exercices={
     starting :{
@@ -68,26 +69,38 @@ function changeText(){
     text.textContent= desc
 }
 
+
 function startCycle(){
     if (text.textContent != ""){
         let selectedExercice = exercices[selectedContainer]
         const {inhaleSec, exhaleSec,wait}= selectedExercice 
-        let iteration = 0;
-        let numberOfIteration = ((5*60)/(inhaleSec+exhaleSec));
+        let timeOfIteration = (inhaleSec+exhaleSec)
+        let numberOfIteration = (((5*60)/timeOfIteration)+1);
+
         startInhaleAnimation(inhaleSec);
+        updateTracker(iteration,numberOfIteration)
         circle.addEventListener('animationend',(event)=>{
-            if (iteration < numberOfIteration){
-                let animationName = event.animationName;
-                if (animationName == 'circleInhale'){   
-                    startExhaleAnimation(exhaleSec);
-                    iteration++
-                    updateTracker(iteration,numberOfIteration)
-                }
-                else if (animationName == 'circleExhale'){
-                    startInhaleAnimation(inhaleSec);
-                }
-            }
-        })
+            testIteration(
+                numberOfIteration,
+                inhaleSec,
+                exhaleSec,
+                event
+            );
+        })    
+    }
+}
+
+function testIteration(numberOfIteration,inhaleSec,exhaleSec,event){
+    if (iteration < numberOfIteration){
+        let animationName = event.animationName;
+        if (animationName == 'circleInhale'){   
+            startExhaleAnimation(exhaleSec);
+            iteration++
+            updateTracker(iteration,numberOfIteration)
+        }
+        else if (animationName == 'circleExhale'){
+            startInhaleAnimation(inhaleSec);
+        }
     }
 }
 
@@ -99,9 +112,10 @@ function startExhaleAnimation(exSec){
     circle.style.animation="none";
     circle.style.animation=`circleExhale ${exSec}s forwards`;
 }
-btnStart.addEventListener("click",startCycle);
 
 function updateTracker(iteration,numberOfIteration){
     let width = (iteration/numberOfIteration)*100
     progress.style.setProperty("--progress", `${width}%`)
 }
+
+btnStart.addEventListener("click",startCycle);
